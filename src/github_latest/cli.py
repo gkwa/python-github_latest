316826ccb631fd3ee18f87ad84d88114adb0415b
Your Name <you@example.com>
2021-10-23 15:18:42 -0700

    Add --log debug or --log info switch

--- src/github_latest/cli.py
+++ src/github_latest/cli.py
@@ -34,13 +34,36 @@ def main(argv=sys.argv):
 
     Does stuff.
     """
-    monacelli_pylog_prefs.logger.setup(stream_level=logging.INFO)
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "url", help="example https://github.com/mozilla/sops/releases/latest"
     )
+
+    parser.add_argument(
+        "-l",
+        "--log",
+        dest="logLevel",
+        default="INFO",
+        choices=[
+            "DEBUG",
+            "INFO",
+            "WARNING",
+            "ERROR",
+            "CRITICAL",
+            "debug",
+            "info",
+            "warning",
+            "error",
+            "critical",
+        ],
+        help="Set the logging level",
+    )
+
     args = parser.parse_args()
+
+    monacelli_pylog_prefs.logger.setup(stream_level=args.logLevel.upper())
+
     response = requests.get(args.url)
     path = pathlib.Path(response.url)
     vorig = path.name
