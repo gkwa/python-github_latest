cf002c2da90c2c191c7f50051cbeea925c2179d0
Your Name <you@example.com>
2021-10-23 23:07:33 -0700

    Bump version: 0.3.2 → 0.4.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*names, **kwargs):
 
 setup(
     name="github-latest",
-    version='0.3.2',
+    version='0.4.0',
     license="BSD-2-Clause",
     description="An example package. Generated with cookiecutter-pylibrary.",
     long_description="%s\n%s"
