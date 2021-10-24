8570a8742d216f9df08b5ade6315ae9dfd7ca0d8
Your Name <you@example.com>
2021-10-23 22:18:15 -0700

    refactor

--- src/github_latest/cli.py
+++ src/github_latest/cli.py
@@ -15,7 +15,6 @@ Why does this file exist, and why not put this in __main__?
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 
-import logging
 import sys
 
 import monacelli_pylog_prefs.logger
