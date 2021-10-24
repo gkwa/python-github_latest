9c77b56c8ae206998c712b054b78836e8de5c4f6
Your Name <you@example.com>
2021-10-23 21:52:30 -0700

    Bump version: 0.0.7 → 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "github_latest"
 year = "2021"
 author = "Taylor Monacelli"
 copyright = "{0}, {1}".format(year, author)
-version = release = '0.0.7'
+version = release = '0.1.0'
 
 pygments_style = "trac"
 templates_path = ["."]
